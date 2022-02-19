0990425270ace7a339954c7df982d4f3e4b16d04
Victor Hugo Oliveira da Silva <victorsilva698@gmail.com>
2022-02-19 01:27:22 -0300

    add other asserts types

--- intro.py
+++ intro.py
@@ -7,15 +7,22 @@ from pyrsistent import b
 # escrevendo testes unitário / writing unit tests
 def soma(a,b):
     return a+b
-def subtracao(a,b):
-    return a-b
-def multiplicacao(a,b):
-    return a*b
-    
+
+test_number_list = [1,2,3]
+
+def higherThan(higher,lower):
+    return higher > lower
+
 class TestClass(unittest.TestCase):
     def test_soma(self):
         result = soma(1,3)
         error_msg = f"{result} is not equal to 3"
-        self.assertEqual(result,3,error_msg)
+        self.assertNotEqual(result,3,error_msg)
+        
+    def test_if_in(self):
+        self.assertIn(2,test_number_list,"Não está na lista")
+        
+    def teste_higher_than(self):
+        self.assertTrue(higherThan(3,2),"First num not higher than the second")
         
 unittest.main()
